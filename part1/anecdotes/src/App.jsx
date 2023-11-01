aea3e8d49357e2480885185e97d75ec01fc08138
Miikka K <miikkak@tuta.io>
2023-11-01 13:55:04 -0600

    adds anecdotes

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,72 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+
+const Button = ({ onClick, label }) => (
+  <button onClick={onClick}>{label}</button>
+);
+
+const Anecdote = ({ text, votes }) => {
+  return (
+    <div>
+      {text}
+      <p>has {votes} votes</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+
+  const mostVotedIndex = votes.indexOf(Math.max(...votes));
+
+  const handleRandomAnecdote = () => {
+    const randomNumber = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomNumber);
+  };
+
+  const handleVoteClick = () => {
+    let newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+  };
+
+  const buttons = [
+    { onClick: handleVoteClick, label: "vote" },
+    { onClick: handleRandomAnecdote, label: "next anecdote" },
+  ];
+
+  return (
+    <>
+      <h1>Anectode of the day</h1>
+      <Anecdote text={anecdotes[selected]} votes={votes[selected]} />
+
+      {buttons.map((button, i) => (
+        <Button
+          key={button + i}
+          onClick={button.onClick}
+          label={button.label}
+        />
+      ))}
+
+      <h1>Anecdote with most votes</h1>
+      <Anecdote
+        text={anecdotes[mostVotedIndex]}
+        votes={votes[mostVotedIndex]}
+      />
+    </>
+  );
+};
+
+export default App;
