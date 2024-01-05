674f2004c94f57f0d4e4bf373aac13f4f28cd57e
Miikka K <miikkak@tuta.io>
2024-01-04 22:24:09 -0600

    pt2 setup for pt3 backend

---
+++ part3/example/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello!</h1>");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find((note) => note.id === id);
+  console.log(note);
+
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
