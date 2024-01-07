560583954ce99fa420a135ced57284dabd8205ea
Miikka K <miikkak@tuta.io>
2024-01-06 20:47:38 -0600

    4.13-4.14

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -6,23 +6,51 @@ blogsRouter.get("/", async (request, response) => {
   response.json(blogs);
 });
 
+blogsRouter.get("/:id", async (request, response) => {
+  const id = request.params.id;
+  const blog = await Blog.findById(id);
+  console.log("get by id", blog);
+  response.json(blog);
+});
+
 blogsRouter.post("/", async (request, response) => {
-  const body = request.body;
+  const { title, author, url, likes } = request.body;
 
-  if (body.title === undefined || body.url === undefined) {
-    response.status(400).end();
-  }
-
-  const newBlog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes ? body.likes : 0,
+  const blog = new Blog({
+    title: title,
+    author: author,
+    url: url,
+    likes: likes ? likes : 0,
   });
 
-  const savedBlog = await newBlog.save();
+  if (!title || !url) {
+    response.status(400).json({ error: "missing url and/or title" }).end();
+  } else {
+    const savedBlog = await blog.save();
+    response.status(201).json(savedBlog);
+  }
+});
 
-  response.status(201).json(savedBlog);
+blogsRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id);
+  response.status(204).end();
+});
+
+blogsRouter.put("/:id", async (request, response) => {
+  const { title, author, url, likes } = request.body;
+
+  const id = request.params.id;
+
+  const result = await Blog.findByIdAndUpdate(id, {
+    title,
+    author,
+    url,
+    likes,
+  });
+
+  const updatedBlog = await Blog.findById(id);
+
+  response.status(200).json(updatedBlog);
 });
 
 module.exports = blogsRouter;
--- part4/blog/tests/blogs_api.test.js
+++ part4/blog/tests/blogs_api.test.js
@@ -10,59 +10,103 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs);
 }, 100000);
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-}, 100000);
+describe("general", () => {
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  }, 100000);
 
-test("all blogs are returned", async () => {
-  const response = await api.get("/api/blogs");
+  test("all blogs are returned", async () => {
+    const response = await api.get("/api/blogs");
 
-  expect(response.body).toHaveLength(helper.initialBlogs.length);
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  }, 100000);
+
+  test("unique identifier is id", async () => {
+    const response = await helper.blogsInDb();
+
+    const blogToCheck = response[0];
+
+    expect(blogToCheck.id).toBeDefined();
+  });
 });
 
-test("unique identifier is id", async () => {
-  const response = await helper.blogsInDb();
+describe("creating", () => {
+  test("post req works and creates new blog post", async () => {
+    const newPost = {
+      title: "Testing a new post",
+      author: "Jesus",
+      url: "http://cleancoder.com/",
+    };
 
-  const blogToCheck = response[0];
+    await api.post("/api/blogs").send(newPost).expect(201);
+    const newBlogs = await api.get("/api/blogs");
+    expect(newBlogs.body).toHaveLength(helper.initialBlogs.length + 1);
+  }, 100000);
 
-  expect(blogToCheck.id).toBeDefined();
+  test("new post missing likes defaults to zero", async () => {
+    const newPost = {
+      author: "steve",
+      url: "someurl.com",
+      title: "sometitle",
+    };
+
+    await api.post("/api/blogs").send(newPost).expect(201);
+
+    const newBlogs = await helper.blogsInDb();
+    const lastOne = newBlogs.at(-1);
+    expect(lastOne.likes).toBe(0);
+  }, 100000);
+
+  test("new post missing url or title", async () => {
+    const newPost = {
+      author: "Rick",
+    };
+
+    await api.post("/api/blogs").send(newPost).expect(400);
+  });
 });
 
-test("post req works and creates new blog post", async () => {
-  const newPost = {
-    title: "Testing a new post",
-    author: "Jesus",
-    url: "http://cleancoder.com/",
-  };
+describe("removing", () => {
+  test("delete a blog post succesfully", async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
 
-  await api.post("/api/blogs").send(newPost).expect(201);
-  const newBlogs = await api.get("/api/blogs");
-  expect(newBlogs.body).toHaveLength(helper.initialBlogs.length + 1);
-}, 100000);
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
 
-test("new post missing likes defaults to zero", async () => {
-  const newPost = {
-    author: "steve",
-    url: "someurl.com",
-    title: "sometitle",
-  };
+    const blogsAtEnd = await helper.blogsInDb();
 
-  await api.post("/api/blogs").send(newPost).expect(201);
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
 
-  const newBlogs = await helper.blogsInDb();
-  const lastOne = newBlogs.at(-1);
-  expect(lastOne.likes).toBe(0);
+    const contents = blogsAtEnd.map((x) => x.title);
+
+    expect(contents).not.toContain(blogToDelete.content);
+  });
 });
 
-test("new post missing title or url returns 400 bad request", async () => {
-  const badPost = {
-    author: "Jesus",
-  };
+describe("editing", () => {
+  test("editing a post succesfully", async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToEdit = blogsAtStart[1];
 
-  await api.post("/api/blogs").send(badPost).expect(400);
+    const newBlog = {
+      title: blogToEdit.title,
+      author: blogToEdit.author,
+      url: blogToEdit.url,
+      likes: 69420,
+    };
+    await api.put(`/api/blogs/${blogToEdit.id}`).send(newBlog).expect(200);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+
+    const blogToEditEnd = blogsAtEnd[1];
+
+    expect(blogToEditEnd.likes).not.toBe(blogToEdit.likes);
+  }, 100000);
 });
 
 afterAll(async () => {
